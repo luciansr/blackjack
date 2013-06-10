f9d69b1aef46283487ed753246d3bbb434c48610
luciansr <luciansr@gmail.com>
2013-06-10 00:21:04 -0300

    controller é criado com lista de jogadores

--- src/controller/BlackJackController.java
+++ src/controller/BlackJackController.java
@@ -1,8 +1,12 @@
 package controller;
 
+import java.util.ArrayList;
+
+import model.*;
+
 public class BlackJackController {
 	
-	public BlackJackController() {
+	public BlackJackController(ArrayList<Player> listaJogadores) {
 		// TODO Auto-generated constructor stub
 	}
 
